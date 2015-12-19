c9594122d461b20102c56628a4fa2d426252dc5f
theWildSushii <sushiiito123@gmail.com>
2015-12-18 22:20:52 -0700

    Slight UI changes; added 2 sidebar items

--- Navi/script.js
+++ Navi/script.js
@@ -14,12 +14,12 @@ window.onload = init;
 var isShowing = false;
 function showList(){
   if(isShowing){
-    e("list").style.overflow = "hidden";
+    e("list").style.display = "none";
     e("list").style.width = "0px";
     e("settings").style.marginRight = "8px";
     e("list").style.backgroundColor = "rgba(0, 255, 255, 0)"
   } else {
-    e("list").style.overflow = "scroll";
+    e("list").style.display = "block";
     e("list").style.width = "61.8%";
     e("settings").style.marginRight = "61.8%";
     e("list").style.backgroundColor = "rgba(0, 255, 255, .64)"
@@ -49,8 +49,11 @@ function textCheck(e){
 
 function sendMessage() {
   var text = e("message").value;
+  for(var i = 0; i < text.length; i++){
+    text = text.replace("\n", " ");
+  }
   e("message").value = "";
-  if(text != "" || text != "\n" || text != "\n\n"){
+  if(text != "" || text != " " || text != "  "){
     e("message").focus();
     popUp("user", text);
     loaded(false);
@@ -70,8 +73,15 @@ function randInt(min, max) {
 
 var messageCount = 0;
 
-function popUp(user, text) {
+function popUp(user, rawText) {
+  var text = rawText;
   if(text != ""){
+    if(user == "user" || user == "navi"){
+      for(var i = 0; i < text.split("").length; i++){
+        text = text.replace("<", "&lt;");
+        text = text.replace(">", "&gt;");
+      }
+    }
     var id = "m" + messageCount;
     var input = e("input");
     input.insertAdjacentHTML('beforebegin', "<div id=\"" + id + "\" class=\"" + user + "\"><p>" + text + "</p></div>");
