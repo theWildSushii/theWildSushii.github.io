21233d579fcbbdb569814ec895e7960d5d855f3e
theWildSushii <sushiiito123@gmail.com>
2018-03-04 22:06:02 -0700

    Fully remake

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function e(x) {
+    return document.getElementById(x);
+}
+
 function popUpImg(img){
   e("popup-container").innerHTML = "<div id=\"popup-img\" onclick=\"closepopup()\"><img src=\"" + img.src + "\"></div>";
 }
